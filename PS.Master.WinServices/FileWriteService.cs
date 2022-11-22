e877e27b095dcd699fd69eadccc1929aa626f421
phetanshi <padmasekhar.pottepalem@outlook.com>
2022-11-22 07:53:21 +0530

    Added Windows service

---
+++ PS.Master.WinServices/FileWriteService.cs
@@ -0,0 +1,61 @@
+﻿using System.ServiceProcess;
+using System.Threading;
+
+namespace PS.Master.WinServices
+{
+    public class FileWriteService : ServiceBase
+    {
+        public Thread Worker = null;
+        public FileWriteService()
+        {
+            ServiceName = "PsLogService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ThreadStart start = new ThreadStart(Working);
+            Worker = new Thread(start);
+            Worker.Start();
+        }
+
+        public void Working()
+        {
+            int nsleep = 1; //interval as 1 minute
+            while (true)
+            {
+                string FileName = @"E:\NewLaptop\PocProjects\dotnet\core\MASTER_APP_POC\WinServices\CoreWinServiceLog.txt";
+                using(StreamWriter sw = new StreamWriter(FileName, true))
+                {
+                    sw.WriteLine($"Windows Service called on {DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss")}");
+                    sw.Close();
+                }
+                Thread.Sleep(nsleep * 60 * 1000);
+            }
+        }
+        protected override void OnStop()
+        {
+            try
+            {
+                if ((Worker != null) & Worker.IsAlive)
+                {
+                    string FileName = @"E:\NewLaptop\PocProjects\dotnet\core\MASTER_APP_POC\WinServices\CoreWinServiceLog.txt";
+                    using (StreamWriter sw = new StreamWriter(FileName, true))
+                    {
+                        sw.WriteLine($"Windows Service stopped on {DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss")}");
+                        sw.Close();
+                    }
+                    Worker.Abort();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+    }
+}
---
+++ PS.Master.WinServices/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceProcess;
+
+namespace PS.Master.WinServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var service = new FileWriteService())
+            {
+                ServiceBase.Run(service);
+                //service.OnDebug();
+            }
+        }
+    }
+}
